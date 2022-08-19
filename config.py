7b406b32aadf4d086aa404ae587d2c272268af2f
张三 <100053992+1379255913@users.noreply.github.com>
2022-08-19 23:43:11 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ DEBUG = False
 SECRET_KEY = os.urandom(24)
 
 db = pymysql.connect(host='localhost', user='root', password='', db='OnlineForumPlatform', port=3306)
-SQLALCHEMY_DATABASE_URI="mysql://root:1379255913zyy@localhost:3306/OnlineForumPlatform"
+SQLALCHEMY_DATABASE_URI="mysql://root:@localhost:3306/OnlineForumPlatform"
 SQLALCHEMY_TRACK_MODIFICATIONS=False
 TEMPLATES_AUTO_RELOAD =True
 MAIL_SERVER = "smtp.qq.com"
